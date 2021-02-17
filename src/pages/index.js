2d1786f2b5e5f7787d4cc6f811f5bf217193e6a8
Al Brancozzi <al.brancozzi@gmail.com>
2021-02-17 17:54:27 +0100

    update font

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import * as React from "react";
 const pageStyles = {
   color: "#232129",
   padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  fontFamily: "Courier, sans-serif, serif",
 };
 const headingStyles = {
   marginTop: 0,
